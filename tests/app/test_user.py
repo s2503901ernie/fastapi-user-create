b34d59aea02be880ec36f7712bd525af3bf1f77a
 黃鴻翔 <s2503901@gmail.com>
2024-05-09 22:32:20 +0800

    feat(password) Add the password verification
    - Add Password validator
    - Add password validator test
    - Add password hash function

--- tests/app/test_user.py
+++ tests/app/test_user.py
@@ -4,10 +4,81 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 
 from sqlalchemy.exc import IntegrityError
 from unittest.mock import Mock
-from unittest.mock import patch
 from models.user import UserActionMessage
 from app.user import create_user
+from app.user import PasswordValidator
+from app.user import UserNameValidator
+from settings import PASSWORD_MAX_LENGTH
+from settings import PASSWORD_MIN_LENGTH
+from settings import USERNAME_MAX_LENGTH
 from settings import USERNAME_MIN_LENGTH
+
+
+def test_UserNameValidator():
+    # Test case 1: Valid successful username
+    name = "test_user"
+    validator = UserNameValidator(name=name)
+    success, msg = validator.validate()
+    assert success is True
+    assert msg == ''
+
+    # Test case 2: Less than the required length
+    name = 'in'
+    validator = UserNameValidator(name=name)
+    success, msg = validator.validate()
+    assert success is False
+    assert msg == f'The length of the user name is too short, should be at least {USERNAME_MIN_LENGTH} characters.'
+
+    # Test case 3: Larger than the required length
+    name = 'n' * 33
+    validator = UserNameValidator(name=name)
+    success, msg = validator.validate()
+    assert success is False
+    assert msg == f'The length of the user name is too long, should be at most {USERNAME_MAX_LENGTH} characters.'
+
+
+def test_PasswordValidator():
+    # Test case 1: Valid successful password
+    password = 'Abc12345678'
+    validator = PasswordValidator(password=password)
+    success, msg = validator.validate()
+    assert success is True
+    assert msg == ''
+
+    # Test case 2: Less than the required length
+    password = 'aA'
+    validator = PasswordValidator(password=password)
+    success, msg = validator.validate()
+    assert success is False
+    assert msg == f'The length of the password is too short, should be at least {PASSWORD_MIN_LENGTH} characters.'
+
+    # Test case 3: Larger than the required length
+    password = 'Abc123' + 'n' * 30
+    validator = PasswordValidator(password=password)
+    success, msg = validator.validate()
+    assert success is False
+    assert msg == f'The length of the password is too long, should be at most {PASSWORD_MAX_LENGTH} characters.'
+
+    # Test case 4: Missing lowercase
+    password = 'ABCDEFGH123'
+    validator = PasswordValidator(password=password)
+    success, msg = validator.validate()
+    assert success is False
+    assert msg == 'The lowercase is missing, should be at least one.'
+
+    # Test case 5: Missing uppercase
+    password = 'abcdefgh123'
+    validator = PasswordValidator(password=password)
+    success, msg = validator.validate()
+    assert success is False
+    assert msg == 'The uppercase is missing, should be at least one.'
+
+    # Test case 6: Missing number
+    password = 'Abcdefgh'
+    validator = PasswordValidator(password=password)
+    success, msg = validator.validate()
+    assert success is False
+    assert msg == 'The number is missing, should be at least one.'
 
 
 def test_create_user():
@@ -16,27 +87,60 @@ def test_create_user():
     session_mock = Mock()
 
     # Test case 1: Valid username and successful user creation
-    response = create_user("test_user", "test_password", session_mock)
+    response = create_user("test_user", "Abc12345678", session_mock)
     assert isinstance(response, UserActionMessage)
     assert response.success is True
     assert response.reason == ''
 
     # Test case 2: Less than the required length
-    response = create_user("in", "test_password", session_mock)
+    response = create_user("in", "Abc12345678", session_mock)
     assert isinstance(response, UserActionMessage)
     assert response.success is False
     assert response.reason == f'The length of the user name is too short, should be at least {USERNAME_MIN_LENGTH} characters.'
 
-    # Test case 3: Duplicated username
-    with patch('db.user.create_db_user') as create_db_user_mock:
-        create_db_user_mock.side_effect = IntegrityError("Duplicate username", params=None, orig=None)
-        response = create_user("duplicated_user", "test_password", session_mock)
-        assert isinstance(response, UserActionMessage)
-        assert response.success is False
-        assert response.reason == 'The username: [duplicated_user] has been created already, please change another one.'
+    # Test case 3: Larger than the required length
+    response = create_user("n" * 33, "Abc12345678", session_mock)
+    assert isinstance(response, UserActionMessage)
+    assert response.success is False
+    assert response.reason == f'The length of the user name is too long, should be at most {USERNAME_MAX_LENGTH} characters.'
 
+    # Test case 4: Duplicated username
+    session_mock.add.side_effect = IntegrityError("Duplicate username", params=None, orig=None)
+    response = create_user("duplicated_user", "Abc12345678", session_mock)
+    assert isinstance(response, UserActionMessage)
+    assert response.success is False
+    assert response.reason == 'The username: [duplicated_user] has been created already, please change another one.'
 
+    # Test case 5: Password is less than the required length
+    response = create_user('test_user', 'Aa', session_mock)
+    assert isinstance(response, UserActionMessage)
+    assert response.success is False
+    assert response.reason == f'The length of the password is too short, should be at least {PASSWORD_MIN_LENGTH} characters.'
 
+    # Test case 6: Larger than the required length
+    password = 'Abc123' + 'n' * 30
+    response = create_user('test_user', password, session_mock)
+    assert isinstance(response, UserActionMessage)
+    assert response.success is False
+    assert response.reason == f'The length of the password is too long, should be at most {PASSWORD_MAX_LENGTH} characters.'
 
+    # Test case 7: Missing lowercase
+    password = 'ABCDEFGH123'
+    response = create_user('test_user', password, session_mock)
+    assert isinstance(response, UserActionMessage)
+    assert response.success is False
+    assert response.reason == 'The lowercase is missing, should be at least one.'
 
+    # Test case 8: Missing uppercase
+    password = 'abcdefgh123'
+    response = create_user('test_user', password, session_mock)
+    assert isinstance(response, UserActionMessage)
+    assert response.success is False
+    assert response.reason == 'The uppercase is missing, should be at least one.'
 
+    # Test case 9: Missing number
+    password = 'Abcdefgh'
+    response = create_user('test_user', password, session_mock)
+    assert isinstance(response, UserActionMessage)
+    assert response.success is False
+    assert response.reason == 'The number is missing, should be at least one.'
